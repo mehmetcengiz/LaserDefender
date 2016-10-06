0c817839509548c8d5f16eb97c7626e845382931
Mehmet Cengiz <m.cengiz4716@gmail.com>
2016-10-07 00:20:58 +0300

    *Fire Laser Function Added
    -InvokeRepeating
    -CancelInvoke

--- Assets/Entities/Player/PlayerController.cs
+++ Assets/Entities/Player/PlayerController.cs
@@ -1,9 +1,13 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
     public float Speed=5.0f;
     public float Padding = 0.5f;
+    public GameObject Projectile;
+    public float ProjectileSpeed;
+    public float FiringRate = 0.2f;
 
     private float _xmin;
     private float _xmax;
@@ -16,7 +20,21 @@ public class PlayerController : MonoBehaviour {
         _xmin = leftMost.x + Padding;
         _xmax = rightMost.x - Padding;
     }
+
+    void Fire() {
+        GameObject beam = Instantiate(Projectile, transform.position, Quaternion.identity) as GameObject;
+        beam.GetComponent<Rigidbody2D>().velocity = new Vector3(0, ProjectileSpeed, 0);
+    }
+
     void Update() {
+
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            InvokeRepeating("Fire",0.00001f,FiringRate);
+        }
+        if (Input.GetKeyUp(KeyCode.Space)) {
+            CancelInvoke("Fire");
+        }
+
         if (Input.GetKey(KeyCode.RightArrow)) {
             transform.position += Vector3.right * Speed * Time.deltaTime;
         }
