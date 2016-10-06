01fe5d010f1ae4ef8c5e28a48fbe3a0a66defc90
Mehmet Cengiz <m.cengiz4716@gmail.com>
2016-10-06 17:49:11 +0300

    *Initial Commit

---
+++ Assets/Entities/EnemyFormation/EnemySpawner.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour {
+
+    public GameObject enemyPrefab;
+    public float Width = 10f;
+    public float Height = 5f;
+    public float speed = 10f;
+
+    private bool _isMovingRight = true;
+    private float _xmax;
+    private float _xmin;
+
+
+	void Start () {
+	    float distanceToCamera = transform.position.z - Camera.main.transform.position.z;
+	    Vector3 leftBoundary = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distanceToCamera));
+        Vector3 rightBoundary = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distanceToCamera));
+	    _xmax = rightBoundary.x;
+	    _xmin = leftBoundary.x;
+        foreach (Transform child in transform) {
+            GameObject enemy = Instantiate(enemyPrefab,child.transform.position, Quaternion.identity) as GameObject;
+            enemy.transform.parent = child;
+        }
+
+	}
+
+    public void OnDrawGizmos() {
+        Gizmos.DrawWireCube(transform.position,new Vector3(Width,Height));    
+    }
+
+	// Update is called once per frame
+	void Update () {
+	    if (_isMovingRight) {
+	        transform.position += Vector3.right * speed * Time.deltaTime;
+	    }
+	    else {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+
+	    float rightEdgeOfFormation = transform.position.x + (0.5f*Width);
+        float leftEdgeOfFormation = transform.position.x - (0.5f* Width);
+
+	    if (leftEdgeOfFormation < _xmin || rightEdgeOfFormation > _xmax) {
+	        _isMovingRight = !_isMovingRight;
+	    }
+
+	}
+}
---
+++ Assets/Entities/Player/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float Speed=5.0f;
+    public float Padding = 0.5f;
+
+    private float _xmin;
+    private float _xmax;
+    
+
+    void Start() {
+        float distance = transform.position.z - Camera.main.transform.position.z;
+        Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0,0,distance));
+        Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1,0, distance));
+        _xmin = leftMost.x + Padding;
+        _xmax = rightMost.x - Padding;
+    }
+    void Update() {
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            transform.position += Vector3.right * Speed * Time.deltaTime;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.position += Vector3.left * Speed * Time.deltaTime;
+        }
+        //restrict the player to the Gamespace
+        float newX = Mathf.Clamp(transform.position.x, _xmin, _xmax);
+        transform.position = new Vector3(newX,transform.position.y,transform.position.z);
+    }
+}
